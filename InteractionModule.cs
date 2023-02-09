94c3c6efcdf8863570a2f88b047437519606d9ab
Fernando Arruda <fernandojs006@gmail.com>
2023-02-09 01:31:56 -0300

    Adicionando imagem

--- InteractionModule.cs
+++ InteractionModule.cs
@@ -1,13 +1,31 @@
-﻿using Discord.Interactions;
-using Discord;
+﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
+using System.Drawing.Printing;
+using Discord.Interactions;
 using Newtonsoft.Json;
 using YamlDotNet.Serialization.NamingConventions;
-
 namespace Hi_bot 
 {
     public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
     {
 
+
+
+        private Bitmap WriteText(string text)
+        {
+            Bitmap image = (Bitmap)Image.FromFile("template.jpg");
+            Graphics graphics = Graphics.FromImage(image);
+            Font font = new Font("Arial", 38);
+            Rectangle textContainer = new Rectangle(370, 510, 256, 128);
+            graphics.FillRectangle(Brushes.Transparent, textContainer);
+            textContainer.Inflate(-10, -10);
+            graphics.DrawString(text, font, Brushes.Black, textContainer);
+
+            return image;
+            
+        }
 
         private async Task<string> SearchGif(string query)
         {
@@ -36,7 +54,6 @@ namespace Hi_bot
 
             return gifUrl;
 
-
         }
 
         [SlashCommand("hi", "esse e bom")]
@@ -45,14 +62,25 @@ namespace Hi_bot
             try
             {
                 string gifUrl = await SearchGif(query);
+                var image = WriteText(texto);
 
-                await RespondAsync($"OMG [{texto}]({gifUrl}) HI!!!");
+                MemoryStream imageStream = new MemoryStream();
+                image.Save(imageStream, ImageFormat.Gif);
+
+                await RespondWithFileAsync(imageStream, "hii.jpg");
             } 
             catch(Exception ex) 
             {
                 Console.WriteLine(ex);
             }
 
+        }
+
+        [SlashCommand("template", "Responde com a imagem de template.")]
+        public async Task HandleImageCommand(string text)
+        { 
+            await RespondWithFileAsync("template.jpg");
+            
         }
 
     }
